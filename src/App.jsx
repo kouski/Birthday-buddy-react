a8f4929283d331d374488db398c1de2ad1c7b7be
kouski <72550619+kouski@users.noreply.github.com>
2025-01-30 21:43:10 +0100

    renderizar data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import { useState } from 'react';
+import data from './data.js'
+
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
+
+  const[people,setPeople] = useState(data)
+  return(
+    <main>
+    <div className='container'>
+      <h4>Five birthdays today</h4>
+      
+    {people.map(person=>
+      <div key={person.name}>
+        <img src={person.image} alt="" />
+        <h4>{person.name}</h4>
+        <p>{person.age}</p>
+        </div>
+    )}
+    
+    </div>
+    </main>
+  )}
+  
+  
 export default App;
