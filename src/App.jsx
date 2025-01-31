ba32604024880af7fc520b2f5d435d14b1a89abf
kouski <72550619+kouski@users.noreply.github.com>
2025-01-31 19:19:49 +0100

    funcion handleClick para eliminar personas

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,31 @@ import data from './data.js'
 
 const App = () => {
 
+  function handleClick(){
+    setPeople([])
+  }
+
   const[people,setPeople] = useState(data)
   return(
     <main>
-    <div className='container'>
-      <h4>Five birthdays today</h4>
+    <section className='container'>
+      <h4 id='title'>{people.length} birthdays today</h4>
       
     {people.map(person=>
-      <div key={person.name}>
+      <div key={person.name} className='person'>
         <img src={person.image} alt="" />
+        <div>
         <h4>{person.name}</h4>
         <p>{person.age}</p>
         </div>
+        
+        </div>
+        
     )}
+    <button  onClick={handleClick} type='button'  className='btn btn-block'>Clear All</button>
     
-    </div>
+    </section>
+    
     </main>
   )}
   
